7f534203e4fb2d1294800fd562b04fc5744aebd6
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-03 21:02:18 +0200

    Phase 8.75: extract HTTP source list into make include

---
+++ mk/http-sources.mk
@@ -0,0 +1,7 @@
+HTTP_SRC := \
+        core/http/src/MockHttpClient.cpp \
+
+HTTP_SERVER_SRC := \
+        core/http/src/BasicHttpClient.cpp \
+        core/http/src/SimpleHttpListener.cpp \
+        core/http/src/TestHttpServer.cpp
