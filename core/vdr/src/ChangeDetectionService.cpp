89f7f6c8ef79a6287013a279c4f8a6011ca5ad96
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-03 22:47:27 +0200

    Phase 8.84: add ChangeDetectionService implementation

---
+++ core/vdr/src/ChangeDetectionService.cpp
@@ -0,0 +1,30 @@
+#include "ChangeDetectionService.h"
+
+std::vector<VdrChangeEvent> ChangeDetectionService::detectChanges(
+    const VdrChangeState& previous,
+    const VdrChangeState& current) const
+{
+    std::vector<VdrChangeEvent> events;
+
+    if (previous.statusVersion != current.statusVersion) {
+        events.emplace_back(VdrChangeType::StatusChanged);
+    }
+
+    if (previous.channelsVersion != current.channelsVersion) {
+        events.emplace_back(VdrChangeType::ChannelsChanged);
+    }
+
+    if (previous.recordingsVersion != current.recordingsVersion) {
+        events.emplace_back(VdrChangeType::RecordingsChanged);
+    }
+
+    if (previous.timersVersion != current.timersVersion) {
+        events.emplace_back(VdrChangeType::TimersChanged);
+    }
+
+    if (previous.eventsVersion != current.eventsVersion) {
+        events.emplace_back(VdrChangeType::EventsChanged);
+    }
+
+    return events;
+}
