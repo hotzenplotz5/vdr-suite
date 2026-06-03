2eb6ca4df411be15112841e73cba085355dbe7c2
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-03 22:37:47 +0200

    Phase 8.83: add VdrChangeEvent domain header

---
+++ core/vdr/include/VdrChangeEvent.h
@@ -0,0 +1,25 @@
+#ifndef VDR_CHANGE_EVENT_H
+#define VDR_CHANGE_EVENT_H
+
+#include <string>
+
+enum class VdrChangeType {
+    StatusChanged,
+    ChannelsChanged,
+    RecordingsChanged,
+    TimersChanged,
+    EventsChanged
+};
+
+class VdrChangeEvent {
+public:
+    explicit VdrChangeEvent(VdrChangeType type);
+
+    VdrChangeType type() const;
+    std::string typeName() const;
+
+private:
+    VdrChangeType type_;
+};
+
+#endif
