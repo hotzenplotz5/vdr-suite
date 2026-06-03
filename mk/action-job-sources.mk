84ec85973252381a27a0ac45bdb5693f318ccf49
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-03 21:07:41 +0200

    Phase 8.78: extract action and job source lists into make include

---
+++ mk/action-job-sources.mk
@@ -0,0 +1,30 @@
+ACTIONS_SRC := \
+        core/recordings/src/RecordingActionUtils.cpp
+
+ACTION_SERVICE_SRC := \
+        core/recordings/src/ActionService.cpp \
+        core/recordings/src/RecordingActionUtils.cpp
+
+JOB_SERVICE_SRC := \
+        core/recordings/src/JobService.cpp \
+        core/recordings/src/RecordingActionUtils.cpp
+
+JOB_REPOSITORY_SRC := \
+        core/recordings/src/JobRepository.cpp \
+        core/recordings/src/JobService.cpp \
+        core/recordings/src/RecordingActionUtils.cpp
+
+WORKFLOW_SRC := \
+        core/recordings/src/RecordingWorkflowService.cpp \
+        core/recordings/src/ActionService.cpp \
+        core/recordings/src/JobService.cpp \
+        core/recordings/src/JobRepository.cpp \
+        core/recordings/src/RecordingActionUtils.cpp
+
+WORKER_SRC := \
+        core/recordings/src/WorkerSimulator.cpp
+
+RECTOOLS_ADAPTER_SRC := \
+        core/recordings/src/RectoolsAdapter.cpp \
+        core/recordings/src/JobService.cpp \
+        core/recordings/src/RecordingActionUtils.cpp
