52f18f69849b84189d48e7f2fe749de6c63f5e7b
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-03 20:37:53 +0200

    Phase 8.72: extract VDR source list into make include

---
+++ mk/vdr-sources.mk
@@ -0,0 +1,16 @@
+VDR_SRC := \
+        core/vdr/src/VdrConfig.cpp \
+        core/vdr/src/ExternalVdrAdapter.cpp \
+        core/vdr/src/MockVdrAdapter.cpp \
+        core/vdr/src/VdrService.cpp \
+        core/vdr/src/RestfulApiChannelMapper.cpp \
+        core/vdr/src/RestfulApiRecordingMapper.cpp \
+        core/vdr/src/RestfulApiTimerMapper.cpp \
+        core/vdr/src/RestfulApiEventMapper.cpp \
+        core/vdr/src/RestfulApiStatusMapper.cpp \
+        core/vdr/src/RestfulApiVdrAdapter.cpp \
+        core/vdr/src/VdrOverviewService.cpp \
+        core/vdr/src/VdrOverviewJsonSerializer.cpp \
+        core/vdr/src/VdrSnapshotBuilder.cpp \
+        core/vdr/src/PollingService.cpp \
+        core/vdr/src/VdrAdapterFactory.cpp
