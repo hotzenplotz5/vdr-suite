1460051d16016616a52e0c569d8a5556ce9baca1
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-03 21:04:30 +0200

    Phase 8.77: extract recording source lists into make include

---
+++ mk/recording-sources.mk
@@ -0,0 +1,20 @@
+RECORDINGS_SRC := \
+        core/recordings/src/RecordingRepository.cpp \
+        core/recordings/src/MetadataRepository.cpp \
+        core/recordings/src/RecordingService.cpp
+
+METADATA_SRC := \
+        core/recordings/src/MetadataRepository.cpp \
+        core/recordings/src/MetadataService.cpp
+
+JOB_DASHBOARD_SRC := \
+        core/recordings/src/JobRepository.cpp \
+        core/recordings/src/JobDashboardService.cpp
+
+RECORDING_DASHBOARD_SRC := \
+        core/recordings/src/RecordingRepository.cpp \
+        core/recordings/src/MetadataRepository.cpp \
+        core/recordings/src/RecordingDashboardService.cpp
+
+DASHBOARD_JSON_SRC := \
+        core/recordings/src/DashboardJsonSerializer.cpp
