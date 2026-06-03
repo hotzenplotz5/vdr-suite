2b1d376e0204a354b26e489b9ccf0fddd14b0afc
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-03 21:03:34 +0200

    Phase 8.76: extract daemon source list into make include

---
+++ mk/daemon-sources.mk
@@ -0,0 +1,20 @@
+DAEMON_SRC := \
+        core/daemon/src/RuntimeConfig.cpp \
+        core/recordings/src/JobRepository.cpp \
+        core/recordings/src/JobDashboardService.cpp \
+        core/recordings/src/RecordingRepository.cpp \
+        core/recordings/src/MetadataRepository.cpp \
+        core/recordings/src/RecordingDashboardService.cpp \
+        core/recordings/src/DashboardFacade.cpp \
+        core/recordings/src/DashboardJsonSerializer.cpp \
+        api/rest/src/DashboardController.cpp \
+        api/rest/src/JobsController.cpp \
+        api/rest/src/RecordingsController.cpp \
+        api/rest/src/MetadataController.cpp \
+        api/rest/src/VdrController.cpp \
+        api/rest/src/ApiRouter.cpp \
+        core/http/src/BasicHttpClient.cpp \
+        core/http/src/SimpleHttpListener.cpp \
+        core/http/src/TestHttpServer.cpp \
+        core/daemon/src/DaemonRuntime.cpp \
+        core/daemon/src/DaemonApp.cpp
