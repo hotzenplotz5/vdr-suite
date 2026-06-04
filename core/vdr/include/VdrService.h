4deec153549d223ddcf4ca084b4cea62604de1b1
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-04 08:45:07 +0200

    Phase 8.90: expose change state through VdrService

--- core/vdr/include/VdrService.h
+++ core/vdr/include/VdrService.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "IVdrAdapter.h"
+#include "VdrChangeState.h"
 #include "VdrChannel.h"
 #include "VdrEvent.h"
 #include "VdrRecording.h"
@@ -19,6 +20,7 @@ public:
     std::vector<VdrEvent> getEvents() const;
     std::vector<VdrTimer> getTimers() const;
     std::vector<VdrRecording> getRecordings() const;
+    VdrChangeState getChangeState() const;
 
 private:
     IVdrAdapter& adapter_;
