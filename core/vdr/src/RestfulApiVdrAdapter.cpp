5fab49d150c0687ba48bcca279631d821e20cdbd
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-04 06:24:00 +0200

    Phase 8.88: read RESTfulAPI change-state endpoint

--- core/vdr/src/RestfulApiVdrAdapter.cpp
+++ core/vdr/src/RestfulApiVdrAdapter.cpp
@@ -8,6 +8,49 @@
 #include "RestfulApiStatusMapper.h"
 #include "RestfulApiTimerMapper.h"
 
+#include <cctype>
+#include <string>
+
+namespace {
+int parseIntField(const std::string& json, const std::string& fieldName)
+{
+    const std::string quotedField = "\"" + fieldName + "\"";
+    const std::size_t fieldPosition = json.find(quotedField);
+
+    if (fieldPosition == std::string::npos) {
+        return 0;
+    }
+
+    const std::size_t colonPosition = json.find(':', fieldPosition + quotedField.size());
+
+    if (colonPosition == std::string::npos) {
+        return 0;
+    }
+
+    std::size_t valueStart = colonPosition + 1;
+
+    while (valueStart < json.size() && std::isspace(static_cast<unsigned char>(json[valueStart]))) {
+        ++valueStart;
+    }
+
+    std::size_t valueEnd = valueStart;
+
+    while (valueEnd < json.size() && std::isdigit(static_cast<unsigned char>(json[valueEnd]))) {
+        ++valueEnd;
+    }
+
+    if (valueEnd == valueStart) {
+        return 0;
+    }
+
+    try {
+        return std::stoi(json.substr(valueStart, valueEnd - valueStart));
+    } catch (...) {
+        return 0;
+    }
+}
+}
+
 RestfulApiVdrAdapter::RestfulApiVdrAdapter(VdrConfig config, IHttpClient& httpClient)
     : config_(config),
       httpClient_(httpClient)
@@ -92,6 +135,24 @@ std::vector<VdrRecording> RestfulApiVdrAdapter::getRecordings() const
 
 VdrChangeState RestfulApiVdrAdapter::getChangeState() const
 {
+    HttpRequest request;
+    request.method = "GET";
+    request.url = "/change-state.json";
+    request.headers["Accept"] = "application/json";
+
+    HttpResponse response = httpClient_.execute(request);
+
     VdrChangeState state;
+
+    if (response.statusCode != 200) {
+        return state;
+    }
+
+    state.statusVersion = parseIntField(response.body, "statusVersion");
+    state.channelsVersion = parseIntField(response.body, "channelsVersion");
+    state.recordingsVersion = parseIntField(response.body, "recordingsVersion");
+    state.timersVersion = parseIntField(response.body, "timersVersion");
+    state.eventsVersion = parseIntField(response.body, "eventsVersion");
+
     return state;
 }
