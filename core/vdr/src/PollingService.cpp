e276ce4edc05dec205a1580acdf2c106d85a0b5d
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-04 09:53:21 +0200

    Phase 8.92: recreate polling implementation with refresh decisions

---
+++ core/vdr/src/PollingService.cpp
@@ -0,0 +1,44 @@
+#include "PollingService.h"
+
+#include "VdrService.h"
+#include "VdrSnapshotBuilder.h"
+
+PollingService::PollingService(VdrSnapshotBuilder& snapshotBuilder, VdrService& vdrService)
+    : snapshotBuilder_(snapshotBuilder),
+      vdrService_(vdrService),
+      hasChangeState_(false)
+{
+}
+
+void PollingService::poll()
+{
+    const VdrChangeState currentChangeState = vdrService_.getChangeState();
+
+    changeEvents_.clear();
+
+    if (!hasChangeState_) {
+        snapshot_ = snapshotBuilder_.buildSnapshot();
+        lastChangeState_ = currentChangeState;
+        hasChangeState_ = true;
+        return;
+    }
+
+    changeEvents_ = changeDetectionService_.detectChanges(lastChangeState_, currentChangeState);
+
+    const auto refreshDecision = snapshotRefreshDecisionService_.decide(changeEvents_);
+
+    if (refreshDecision.shouldRefreshSnapshot()) {
+        snapshot_ = snapshotBuilder_.buildSnapshot();
+        lastChangeState_ = currentChangeState;
+    }
+}
+
+const VdrSnapshot& PollingService::snapshot() const
+{
+    return snapshot_;
+}
+
+const std::vector<VdrChangeEvent>& PollingService::changeEvents() const
+{
+    return changeEvents_;
+}
