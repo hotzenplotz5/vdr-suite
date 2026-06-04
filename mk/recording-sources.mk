c2957151322ef185e34816f6afb83cfc3b659a9f
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-04 14:52:31 +0200

    Phase 8.98: complete recording source groups

--- mk/recording-sources.mk
+++ mk/recording-sources.mk
@@ -16,5 +16,22 @@ RECORDING_DASHBOARD_SRC := \
         core/recordings/src/MetadataRepository.cpp \
         core/recordings/src/RecordingDashboardService.cpp
 
+DASHBOARD_FACADE_SRC := \
+        core/recordings/src/JobRepository.cpp \
+        core/recordings/src/JobDashboardService.cpp \
+        core/recordings/src/RecordingRepository.cpp \
+        core/recordings/src/MetadataRepository.cpp \
+        core/recordings/src/RecordingDashboardService.cpp \
+        core/recordings/src/DashboardFacade.cpp
+
 DASHBOARD_JSON_SRC := \
+        core/recordings/src/DashboardJsonSerializer.cpp
+
+DASHBOARD_CLI_SRC := \
+        core/recordings/src/JobRepository.cpp \
+        core/recordings/src/JobDashboardService.cpp \
+        core/recordings/src/RecordingRepository.cpp \
+        core/recordings/src/MetadataRepository.cpp \
+        core/recordings/src/RecordingDashboardService.cpp \
+        core/recordings/src/DashboardFacade.cpp \
         core/recordings/src/DashboardJsonSerializer.cpp
