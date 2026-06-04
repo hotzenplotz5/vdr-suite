79361f097cdd5bb6df4a9a9ced470d53bb170328
Holger Schvestka <hotzenplotz5@gmx.de>
2026-06-05 00:28:42 +0200

    Phase 10.2: add runtime log level formatting

--- core/runtime/src/ConsoleRuntimeLogger.cpp
+++ core/runtime/src/ConsoleRuntimeLogger.cpp
@@ -2,9 +2,35 @@
 
 #include <iostream>
 
+namespace {
+
+const char* toString(RuntimeLogLevel level)
+{
+    switch (level) {
+        case RuntimeLogLevel::Debug:
+            return "DEBUG";
+
+        case RuntimeLogLevel::Info:
+            return "INFO";
+
+        case RuntimeLogLevel::Warning:
+            return "WARNING";
+
+        case RuntimeLogLevel::Error:
+            return "ERROR";
+    }
+
+    return "UNKNOWN";
+}
+
+}
+
 void ConsoleRuntimeLogger::write(const RuntimeLogEntry& entry)
 {
     std::cout
+        << "["
+        << toString(entry.level)
+        << "] "
         << "["
         << entry.component
         << "] "
