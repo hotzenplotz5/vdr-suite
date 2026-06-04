f0de92028136b06d77bcec3d8f387e9044a84ed7
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-04 14:48:31 +0200

    Phase 8.98: add modular Makefile common settings

---
+++ mk/common.mk
@@ -0,0 +1,21 @@
+CXX := g++
+CXXFLAGS := -std=c++17 -Wall -Wextra \
+        -Icore/sqlite/include \
+        -Icore/recordings/include \
+        -Icore/daemon/include \
+        -Icore/vdr/include \
+	-Icore/http/include \
+        -Iapi/rest/include
+
+LDFLAGS := $(shell pkg-config --libs sqlite3)
+
+SQLITE_SRC := core/sqlite/src/Database.cpp
+
+.PHONY: all test clean prepare-test-db dashboard-cli daemon
+
+all: test
+
+prepare-test-db:
+	rm -f /tmp/vdr-suite-test.db
+	sqlite3 /tmp/vdr-suite-test.db < database/schema/vdr-suite.sql
+	sqlite3 /tmp/vdr-suite-test.db < database/testdata/sample-data.sql
