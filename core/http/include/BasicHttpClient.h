c65a9a1366219082d12ba8390ca8a946fd326895
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-05 14:21:42 +0200

    Phase 10.12: add HTTP runtime measurement sink wiring

--- core/http/include/BasicHttpClient.h
+++ core/http/include/BasicHttpClient.h
@@ -2,14 +2,20 @@
 
 #include "IHttpClient.h"
 #include "IRuntimeLogger.h"
+#include "IRuntimeMeasurementSink.h"
 #include "RuntimeLogEntry.h"
 #include "RuntimeLogLevel.h"
+#include "RuntimeMeasurement.h"
 
 #include <string>
 
 class BasicHttpClient : public IHttpClient {
 public:
-    BasicHttpClient(std::string host, int port, IRuntimeLogger* logger = nullptr);
+    BasicHttpClient(
+        std::string host,
+        int port,
+        IRuntimeLogger* logger = nullptr,
+        IRuntimeMeasurementSink* measurementSink = nullptr);
 
     HttpResponse execute(const HttpRequest& request) const override;
 
@@ -17,6 +23,8 @@ private:
     std::string host_;
     int port_;
     IRuntimeLogger* logger_;
+    IRuntimeMeasurementSink* measurementSink_;
 
     void log(RuntimeLogLevel level, const std::string& message) const;
+    void recordMeasurement(const RuntimeMeasurement& measurement) const;
 };
