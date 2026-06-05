fec3cbeb59f3aeb1a71049a116b23f895e447568
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-05 18:40:18 +0200

    Phase 10.14: add polling service measurement sink

--- core/vdr/include/PollingService.h
+++ core/vdr/include/PollingService.h
@@ -2,16 +2,19 @@
 #define POLLING_SERVICE_H
 
 #include "ChangeDetectionService.h"
-#include "SnapshotCacheService.h"
 #include "IRuntimeLogger.h"
+#include "IRuntimeMeasurementSink.h"
 #include "RuntimeLogEntry.h"
 #include "RuntimeLogLevel.h"
+#include "RuntimeMeasurement.h"
+#include "SnapshotCacheService.h"
 #include "SnapshotRefreshPlanner.h"
 #include "SnapshotUpdatePlan.h"
 #include "VdrChangeEvent.h"
 #include "VdrChangeState.h"
 #include "VdrSnapshot.h"
 
+#include <string>
 #include <vector>
 
 class VdrService;
@@ -23,7 +26,8 @@ public:
         VdrSnapshotBuilder& snapshotBuilder,
         VdrService& vdrService,
         SnapshotCacheService& snapshotCacheService,
-        IRuntimeLogger* logger = nullptr);
+        IRuntimeLogger* logger = nullptr,
+        IRuntimeMeasurementSink* measurementSink = nullptr);
 
     void poll();
 
@@ -36,8 +40,10 @@ private:
     VdrService& vdrService_;
     SnapshotCacheService& snapshotCacheService_;
     IRuntimeLogger* logger_;
+    IRuntimeMeasurementSink* measurementSink_;
 
     void log(RuntimeLogLevel level, const std::string& message) const;
+    void recordMeasurement(const RuntimeMeasurement& measurement) const;
     ChangeDetectionService changeDetectionService_;
     SnapshotRefreshPlanner snapshotRefreshPlanner_;
     VdrChangeState lastChangeState_;
