ab738d2dd797eca5afc03099e63f94b3a293c895
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-26 11:03:39 +0200

    Phase 54.3: add preview EPG input metadata model

--- core/vdr/include/SearchTimerPreviewResult.h
+++ core/vdr/include/SearchTimerPreviewResult.h
@@ -3,13 +3,35 @@
 #include "EpgSearchResult.h"
 #include "SearchTimer.h"
 
+#include <string>
+#include <utility>
+#include <vector>
+
 class SearchTimerPreviewResult {
 public:
     SearchTimerPreviewResult(
         SearchTimer searchTimer,
         EpgSearchResult searchResult)
+        : SearchTimerPreviewResult(
+              std::move(searchTimer),
+              std::move(searchResult),
+              "ready",
+              true,
+              {})
+    {
+    }
+
+    SearchTimerPreviewResult(
+        SearchTimer searchTimer,
+        EpgSearchResult searchResult,
+        std::string epgInputStatus,
+        bool epgInputAvailable,
+        std::vector<std::string> warnings)
         : searchTimer_(std::move(searchTimer)),
-          searchResult_(std::move(searchResult))
+          searchResult_(std::move(searchResult)),
+          epgInputStatus_(std::move(epgInputStatus)),
+          epgInputAvailable_(epgInputAvailable),
+          warnings_(std::move(warnings))
     {
     }
 
@@ -21,6 +43,21 @@ public:
     const EpgSearchResult& searchResult() const
     {
         return searchResult_;
+    }
+
+    const std::string& epgInputStatus() const
+    {
+        return epgInputStatus_;
+    }
+
+    bool epgInputAvailable() const
+    {
+        return epgInputAvailable_;
+    }
+
+    const std::vector<std::string>& warnings() const
+    {
+        return warnings_;
     }
 
     int totalCount() const
@@ -41,4 +78,7 @@ public:
 private:
     SearchTimer searchTimer_;
     EpgSearchResult searchResult_;
+    std::string epgInputStatus_;
+    bool epgInputAvailable_ = true;
+    std::vector<std::string> warnings_;
 };
