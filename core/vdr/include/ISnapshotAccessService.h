013c3c3b2e09eacebcb9f35959a4025d3853b30d
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-26 09:11:29 +0200

    Phase 54.3: expose preview EPG cache through snapshot access interface

--- core/vdr/include/ISnapshotAccessService.h
+++ core/vdr/include/ISnapshotAccessService.h
@@ -1,6 +1,7 @@
 #ifndef I_SNAPSHOT_ACCESS_SERVICE_H
 #define I_SNAPSHOT_ACCESS_SERVICE_H
 
+#include "SearchTimerPreviewEpgCache.h"
 #include "VdrSnapshot.h"
 
 #include <string>
@@ -16,6 +17,9 @@ public:
     virtual bool hasSnapshotForBackend(const std::string& backendId) const = 0;
     virtual const VdrSnapshot* snapshotForBackend(const std::string& backendId) const = 0;
     virtual std::vector<VdrSnapshot> snapshots() const = 0;
+
+    virtual SearchTimerPreviewEpgCache& searchTimerPreviewEpgCache() = 0;
+    virtual const SearchTimerPreviewEpgCache& searchTimerPreviewEpgCache() const = 0;
 };
 
 #endif
