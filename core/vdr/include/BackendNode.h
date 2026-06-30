e6e44ab02edd5c9f950071c9c7d4ee83a830bb2f
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-30 13:57:39 +0200

    Phase 57.1: add backend local access mode

--- core/vdr/include/BackendNode.h
+++ core/vdr/include/BackendNode.h
@@ -10,8 +10,19 @@ struct BackendNode
     std::string backendId = "default";
     std::string backendName = "Default VDR";
     std::string backendType = "vdr";
+    std::string accessMode = "read-write";
     VdrConfig connection;
     VdrCapabilitySet capabilities;
     bool enabled = true;
     bool online = false;
+
+    bool readOnly() const
+    {
+        return accessMode == "read-only";
+    }
+
+    bool writeAllowed() const
+    {
+        return !readOnly();
+    }
 };
