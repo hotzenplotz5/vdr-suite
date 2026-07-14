47f6bfc66c39e826ecf207d03e5f5c0b343b73cf
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-14 12:25:39 +0200

    Tests: wire native recording trash workflow

--- mk/recording-trash-tests.mk
+++ mk/recording-trash-tests.mk
@@ -1,7 +1,14 @@
-.PHONY: test-restfulapi-recording-trash-contract
+.PHONY: test-restfulapi-recording-trash-contract \
+	test-restfulapi-recording-trash-workflow
 
 test-restfulapi-recording-trash-contract:
 	$(CXX) $(CXXFLAGS) \
 		core/recordings/tests/test_restfulapi_recording_trash_contract.cpp \
 		-o /tmp/test_restfulapi_recording_trash_contract
 	/tmp/test_restfulapi_recording_trash_contract
+
+test-restfulapi-recording-trash-workflow:
+	$(CXX) $(CXXFLAGS) \
+		core/recordings/tests/test_restfulapi_recording_action_executor_response_contract.cpp \
+		-o /tmp/test_restfulapi_recording_trash_workflow
+	/tmp/test_restfulapi_recording_trash_workflow
