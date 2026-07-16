e2282b1eaa320444ca198396f1b453c19943637c
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-16 22:04:05 +0200

    SB.8: test deterministic continuity payload

--- vdr-plugin-suite-bridge/tests/test_suitebridge_local_contract.cpp
+++ vdr-plugin-suite-bridge/tests/test_suitebridge_local_contract.cpp
@@ -7,43 +7,47 @@
 
 int main()
 {
-  static_assert(SuiteBridgeLocalContractPayload::SchemaVersion() == 1);
-  static_assert(SuiteBridgeLocalContractPayload::Capacity() == 320);
+  static_assert(SuiteBridgeLocalContractPayload::SchemaVersion() == 2);
+  static_assert(SuiteBridgeLocalContractPayload::Capacity() == 448);
   static_assert(
       std::is_copy_constructible<SuiteBridgeLocalContractPayload>::value);
   static_assert(
       !std::is_copy_assignable<SuiteBridgeLocalContractPayload>::value);
 
+  const char *activeEpoch = "0123456789abcdef0123456789abcdef";
   const SuiteBridgeStatusSnapshot activeSnapshot(
       true,
       12,
       2,
       3,
-      4);
+      4,
+      activeEpoch,
+      false);
   const SuiteBridgeLocalContractPayload activePayload(
       1,
       activeSnapshot);
-
   const char *expectedActive =
-      "{\"contract_schema\":1,\"capability_schema\":1,\"snapshot_schema\":1,\"active\":true,\"total\":21,\"channel_switch\":12,\"recording\":2,\"replaying\":3,\"timer_change\":4}";
+      "{\"contract_schema\":2,\"capability_schema\":1,\"snapshot_schema\":2,\"active\":true,\"total\":21,\"channel_switch\":12,\"recording\":2,\"replaying\":3,\"timer_change\":4,\"counter_epoch\":\"0123456789abcdef0123456789abcdef\",\"counter_overflow\":false}";
 
   assert(activePayload.Complete());
   assert(activePayload.Size() == std::strlen(expectedActive));
   assert(activePayload.Size() < activePayload.Capacity());
   assert(std::strcmp(activePayload.Data(), expectedActive) == 0);
 
+  const char *inactiveEpoch = "fedcba9876543210fedcba9876543210";
   const SuiteBridgeStatusSnapshot inactiveSnapshot(
       false,
       0,
       0,
       0,
-      0);
+      0,
+      inactiveEpoch,
+      true);
   const SuiteBridgeLocalContractPayload inactivePayload(
       7,
       inactiveSnapshot);
-
   const char *expectedInactive =
-      "{\"contract_schema\":1,\"capability_schema\":7,\"snapshot_schema\":1,\"active\":false,\"total\":0,\"channel_switch\":0,\"recording\":0,\"replaying\":0,\"timer_change\":0}";
+      "{\"contract_schema\":2,\"capability_schema\":7,\"snapshot_schema\":2,\"active\":false,\"total\":0,\"channel_switch\":0,\"recording\":0,\"replaying\":0,\"timer_change\":0,\"counter_epoch\":\"fedcba9876543210fedcba9876543210\",\"counter_overflow\":true}";
 
   assert(inactivePayload.Complete());
   assert(inactivePayload.Size() == std::strlen(expectedInactive));
