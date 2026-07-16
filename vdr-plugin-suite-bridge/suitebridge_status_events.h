730c8b889cf7abb84cad56a2d25a1cff6a667989
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-16 22:01:27 +0200

    SB.8: attach epoch and saturation to status events

--- vdr-plugin-suite-bridge/suitebridge_status_events.h
+++ vdr-plugin-suite-bridge/suitebridge_status_events.h
@@ -1,10 +1,10 @@
 #ifndef VDR_SUITE_BRIDGE_STATUS_EVENTS_H
 #define VDR_SUITE_BRIDGE_STATUS_EVENTS_H
 
+#include "suitebridge_counter_continuity.h"
 #include "suitebridge_status_snapshot.h"
 
 #include <array>
-#include <atomic>
 #include <cstddef>
 
 enum class SuiteBridgeStatusEventKind : std::size_t {
@@ -21,14 +21,17 @@ public:
 
   unsigned long long Record(SuiteBridgeStatusEventKind kind) noexcept;
   unsigned long long Count(SuiteBridgeStatusEventKind kind) const noexcept;
+  bool CounterOverflowed() const noexcept;
+  const char *CounterEpoch() const noexcept;
   SuiteBridgeStatusSnapshot CaptureSnapshot(bool monitorActive) const noexcept;
 
   static const char *Name(SuiteBridgeStatusEventKind kind) noexcept;
 
 private:
   std::array<
-      std::atomic<unsigned long long>,
+      SuiteBridgeSaturatingCounter,
       static_cast<std::size_t>(SuiteBridgeStatusEventKind::Count)> counts_;
+  const SuiteBridgeCounterEpoch epoch_;
 };
 
 #endif
