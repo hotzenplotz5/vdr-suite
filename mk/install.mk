ec21a3912c4baa0fc4f49d1e101e106463c5a0a6
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-16 06:31:40 +0200

    Build: remove distributed CI group from install rules

--- mk/install.mk
+++ mk/install.mk
@@ -15,8 +15,6 @@ INSTALL ?= install
 
 install: install-runtime install-cli install-docs install-manpages install-systemd
 
-test-ci-fast: test-systemd-unit-contract
-
 test-systemd-unit-contract:
 	python3 tools/check_systemd_unit_contract.py
 
