e7eb43b3d4e0bb80f03e8b9868f21be42033bc84
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-16 22:01:54 +0200

    SB.8: extend snapshot with counter continuity

--- vdr-plugin-suite-bridge/suitebridge_status_snapshot.h
+++ vdr-plugin-suite-bridge/suitebridge_status_snapshot.h
@@ -1,21 +1,31 @@
 #ifndef VDR_SUITE_BRIDGE_STATUS_SNAPSHOT_H
 #define VDR_SUITE_BRIDGE_STATUS_SNAPSHOT_H
 
+#include <array>
+#include <cstddef>
+
 class SuiteBridgeStatusSnapshot final {
 public:
+  static constexpr std::size_t CounterEpochLength() noexcept
+  {
+    return 32;
+  }
+
   SuiteBridgeStatusSnapshot(
       bool monitorActive,
       unsigned long long channelSwitchCount,
       unsigned long long recordingCount,
       unsigned long long replayingCount,
-      unsigned long long timerChangeCount) noexcept;
+      unsigned long long timerChangeCount,
+      const char *counterEpoch,
+      bool counterOverflow) noexcept;
 
   SuiteBridgeStatusSnapshot(const SuiteBridgeStatusSnapshot &) noexcept = default;
   SuiteBridgeStatusSnapshot &operator=(const SuiteBridgeStatusSnapshot &) = delete;
 
   static constexpr unsigned int SchemaVersion() noexcept
   {
-    return 1;
+    return 2;
   }
 
   bool MonitorActive() const noexcept;
@@ -24,13 +34,18 @@ public:
   unsigned long long ReplayingCount() const noexcept;
   unsigned long long TimerChangeCount() const noexcept;
   unsigned long long TotalCount() const noexcept;
+  const char *CounterEpoch() const noexcept;
+  bool CounterOverflow() const noexcept;
 
 private:
-  const bool monitorActive_;
-  const unsigned long long channelSwitchCount_;
-  const unsigned long long recordingCount_;
-  const unsigned long long replayingCount_;
-  const unsigned long long timerChangeCount_;
+  bool monitorActive_;
+  unsigned long long channelSwitchCount_;
+  unsigned long long recordingCount_;
+  unsigned long long replayingCount_;
+  unsigned long long timerChangeCount_;
+  unsigned long long totalCount_;
+  std::array<char, CounterEpochLength() + 1> counterEpoch_;
+  bool counterOverflow_;
 };
 
 #endif
