38dc5524f8191ae004e8994f9cd976962a85de18
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-16 06:19:36 +0200

    Build: remove obsolete recording trash test fragment

--- mk/recording-trash-tests.mk
+++
@@ -1,7 +0,0 @@
-.PHONY: test-restfulapi-recording-trash-contract
-
-test-restfulapi-recording-trash-contract:
-	$(CXX) $(CXXFLAGS) \
-		core/recordings/tests/test_restfulapi_recording_trash_contract.cpp \
-		-o /tmp/test_restfulapi_recording_trash_contract
-	/tmp/test_restfulapi_recording_trash_contract
