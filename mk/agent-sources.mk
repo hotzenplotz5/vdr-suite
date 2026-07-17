e2c76d5071ebc2f4c1158b818d2ff0555c2cdeef
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-17 20:09:39 +0200

    SB.10b: register Agent SVDRP transport source

--- mk/agent-sources.mk
+++ mk/agent-sources.mk
@@ -1,4 +1,11 @@
-AGENT_SRC := \
+AGENT_HANDSHAKE_SRC := \
 	core/agent/src/SuiteBridgeHandshake.cpp \
 	core/agent/src/SuiteBridgeLocalContractParser.cpp \
 	core/agent/src/SuiteBridgeHandshakeService.cpp
+
+AGENT_SVDRP_TRANSPORT_SRC := \
+	core/agent/src/SuiteBridgeSvdrpTransport.cpp
+
+AGENT_SRC := \
+	$(AGENT_HANDSHAKE_SRC) \
+	$(AGENT_SVDRP_TRANSPORT_SRC)
