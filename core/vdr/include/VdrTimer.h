6bbb42b836785351fd33c6697f3975dfee68e6e7
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-18 17:46:17 +0200

    Timer: complete editable read contract

--- core/vdr/include/VdrTimer.h
+++ core/vdr/include/VdrTimer.h
@@ -10,16 +10,21 @@ struct VdrTimer
     std::string eventId;
 
     std::string title;
+    std::string directory;
     std::string subtitle;
     std::string aux;
 
+    std::string day;
+    std::string weekdays = "-------";
     std::string startTime;
     std::string endTime;
 
+    int flags = 0;
     int priority = 0;
     int lifetime = 0;
 
     bool enabled = false;
+    bool vps = false;
     bool recording = false;
     bool pending = false;
 };
