c2dca9955b13dd078fb02f4fef8e9db52f7f4e17
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-18 05:23:58 +0200

    SB.10c: register observation lifecycle sources

--- mk/agent-sources.mk
+++ mk/agent-sources.mk
@@ -6,6 +6,12 @@ AGENT_HANDSHAKE_SRC := \
 AGENT_SVDRP_TRANSPORT_SRC := \
 	core/agent/src/SuiteBridgeSvdrpTransport.cpp
 
+AGENT_OBSERVATION_SRC := \
+	core/agent/src/SuiteBridgeObservation.cpp \
+	core/agent/src/SuiteBridgeObservationService.cpp \
+	core/agent/src/SuiteBridgeObservationWorker.cpp
+
 AGENT_SRC := \
 	$(AGENT_HANDSHAKE_SRC) \
-	$(AGENT_SVDRP_TRANSPORT_SRC)
+	$(AGENT_SVDRP_TRANSPORT_SRC) \
+	$(AGENT_OBSERVATION_SRC)
