d0bd948d87ec8c0e4bd4ea6487bcf22db1557481
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-19 21:05:13 +0200

    Add EPG artwork repository test target

--- mk/vdr-timer-tests.mk
+++ mk/vdr-timer-tests.mk
@@ -112,6 +112,16 @@ test-epg-event-repository:
 	$(BUILD_DIR)/test_epg_event_repository
 
 
+test-epg-artwork-repository:
+	$(BUILD_CXX) $(CXXFLAGS) \
+		$(SQLITE_SRC) \
+		core/vdr/src/EpgArtworkRepository.cpp \
+		core/vdr/tests/test_epg_artwork_repository.cpp \
+		$(LDFLAGS) \
+		-o $(BUILD_DIR)/test_epg_artwork_repository
+	$(BUILD_DIR)/test_epg_artwork_repository
+
+
 test-vdr-domain-objects:
 	$(BUILD_CXX) $(CXXFLAGS) \
 		$(SQLITE_SRC) \
