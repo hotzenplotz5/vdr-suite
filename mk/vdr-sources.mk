f6bfcc95ba22e177f3cdff78ab1f6b2e90bc8a79
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-19 21:49:14 +0200

    Link Suite Bridge EPG artwork resolver into VDR sources

--- mk/vdr-sources.mk
+++ mk/vdr-sources.mk
@@ -17,6 +17,7 @@ VDR_SRC := \
         core/vdr/src/VdrRecordingMetadataJsonSerializer.cpp \
         core/vdr/src/EpgArtworkRepository.cpp \
         core/vdr/src/EpgArtworkEnrichmentService.cpp \
+        core/vdr/src/SuiteBridgeEpgArtworkResolver.cpp \
         core/vdr/src/EpgQueryService.cpp \
         core/vdr/src/EpgSearchMatcher.cpp \
         core/vdr/src/EpgSearchResultJsonSerializer.cpp \
