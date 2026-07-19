5c4efac4d0015aa1d81e955ed2885a0f33ca33cb
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-19 23:33:03 +0200

    Use local TVScraper service contract

--- vdr-plugin-suite-bridge/suitebridge_tvscraper_adapter.cpp
+++ vdr-plugin-suite-bridge/suitebridge_tvscraper_adapter.cpp
@@ -1,30 +1,18 @@
 #include "suitebridge_tvscraper_adapter.h"
 
-#include <vdr/epg.h>
+#include "services.h"
 
-#if defined(SUITEBRIDGE_TVSCRAPER_SERVICES_HEADER)
-#include SUITEBRIDGE_TVSCRAPER_SERVICES_HEADER
-#define SUITEBRIDGE_HAVE_TVSCRAPER_SERVICES 1
-#elif __has_include(<vdr/plugins/tvscraper/services.h>)
-#include <vdr/plugins/tvscraper/services.h>
-#define SUITEBRIDGE_HAVE_TVSCRAPER_SERVICES 1
-#elif __has_include(<tvscraper/services.h>)
-#include <tvscraper/services.h>
-#define SUITEBRIDGE_HAVE_TVSCRAPER_SERVICES 1
-#else
-#define SUITEBRIDGE_HAVE_TVSCRAPER_SERVICES 0
-#endif
+#include <vdr/epg.h>
 
 bool SuiteBridgeTvScraperAdapter::Available() noexcept
 {
-  return SUITEBRIDGE_HAVE_TVSCRAPER_SERVICES != 0;
+  return true;
 }
 
 SuiteBridgeArtworkReference
 SuiteBridgeTvScraperAdapter::ResolvePreferredArtwork(
     const cEvent &event) const
 {
-#if SUITEBRIDGE_HAVE_TVSCRAPER_SERVICES
   cGetScraperVideo request(&event, nullptr);
   if (!request.call() || !request.m_scraperVideo) {
     return {};
@@ -48,8 +36,4 @@ SuiteBridgeTvScraperAdapter::ResolvePreferredArtwork(
   reference.width = media.width;
   reference.height = media.height;
   return reference.Valid() ? reference : SuiteBridgeArtworkReference{};
-#else
-  (void)event;
-  return {};
-#endif
 }
