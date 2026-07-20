76ac7c6ed07b47ab0bd8973862a84e090f694755
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-20 20:42:17 +0200

    Add EPG scraper metadata SVDRP contract

---
+++ vdr-plugin-suite-bridge/suitebridge_epg_metadata_contract.h
@@ -0,0 +1,43 @@
+#ifndef VDR_SUITE_BRIDGE_EPG_METADATA_CONTRACT_H
+#define VDR_SUITE_BRIDGE_EPG_METADATA_CONTRACT_H
+
+#include "suitebridge_epg_metadata.h"
+
+#include <array>
+#include <cstddef>
+#include <string>
+
+class SuiteBridgeEpgMetadataRequest final {
+public:
+  SuiteBridgeEpgMetadataRequest(const char *command, const char *option);
+
+  bool Handled() const noexcept;
+  bool Valid() const noexcept;
+  const std::string &ChannelId() const noexcept;
+  unsigned int EventId() const noexcept;
+
+private:
+  bool handled_ = false;
+  bool valid_ = false;
+  std::string channelId_;
+  unsigned int eventId_ = 0;
+};
+
+class SuiteBridgeEpgMetadataPayload final {
+public:
+  explicit SuiteBridgeEpgMetadataPayload(
+      const SuiteBridgeEpgMetadata &metadata) noexcept;
+
+  const char *Data() const noexcept;
+  std::size_t Size() const noexcept;
+  bool Complete() const noexcept;
+
+private:
+  static constexpr std::size_t kCapacity = 8192;
+
+  std::array<char, kCapacity> data_{};
+  std::size_t size_ = 0;
+  bool complete_ = false;
+};
+
+#endif
