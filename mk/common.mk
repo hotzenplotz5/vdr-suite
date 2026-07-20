9f12828a67aae8985896d0605ea94b63168bc234
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-20 21:34:24 +0200

    Link EPG scraper metadata into cache controller test

--- mk/common.mk
+++ mk/common.mk
@@ -57,6 +57,8 @@ test-epg-cache-controller:
 		core/vdr/src/EpgArtworkEnrichmentService.cpp \
 		core/vdr/src/EpgCacheService.cpp \
 		core/vdr/src/VdrService.cpp \
+		core/vdr/src/EpgScraperMetadataResolverRegistry.cpp \
+		core/vdr/src/EpgScraperMetadataPublicJsonSerializer.cpp \
 		api/rest/src/EpgArtworkController.cpp \
 		api/rest/src/EpgCacheController.cpp \
 		api/rest/tests/test_epg_cache_controller.cpp \
