d983f070836b3a84d08a7da0a99045a23bd7b6c4
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-21 11:37:00 +0200

    Build: add early person context source contract

---
+++ mk/person-context-sources.mk
@@ -0,0 +1,9 @@
+EPG_PERSON_INDEX_SRC := \
+        core/metadata/src/MetadataPlatformSchemaInstaller.cpp \
+        core/vdr/src/EpgPersonIndexRepository.cpp \
+        core/vdr/src/EpgPersonIndexRepositoryMaintenance.cpp \
+        core/vdr/src/EpgPersonEnrichmentService.cpp
+
+PERSON_CONTEXT_SRC := \
+        core/vdr/src/PersonContextService.cpp \
+        core/vdr/src/PersonContextJsonSerializer.cpp
