1e014a7c750001347d49f170e173bee783c0cb6c
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-21 11:37:52 +0200

    Build: consume shared person context source contract

--- mk/vdr-sources.mk
+++ mk/vdr-sources.mk
@@ -1,13 +1,3 @@
-EPG_PERSON_INDEX_SRC := \
-        core/metadata/src/MetadataPlatformSchemaInstaller.cpp \
-        core/vdr/src/EpgPersonIndexRepository.cpp \
-        core/vdr/src/EpgPersonIndexRepositoryMaintenance.cpp \
-        core/vdr/src/EpgPersonEnrichmentService.cpp
-
-PERSON_CONTEXT_SRC := \
-        core/vdr/src/PersonContextService.cpp \
-        core/vdr/src/PersonContextJsonSerializer.cpp
-
 VDR_SRC := \
         $(EPG_PERSON_INDEX_SRC) \
         core/vdr/src/VdrConfig.cpp \
