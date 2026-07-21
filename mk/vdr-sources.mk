29fcefd6c8e4516c7fc6dbeb09e8fb47df833ca5
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-21 11:34:42 +0200

    Build: define explicit EPG person index linkage

--- mk/vdr-sources.mk
+++ mk/vdr-sources.mk
@@ -1,12 +1,15 @@
-PERSON_CONTEXT_SRC := \
+EPG_PERSON_INDEX_SRC := \
         core/metadata/src/MetadataPlatformSchemaInstaller.cpp \
         core/vdr/src/EpgPersonIndexRepository.cpp \
         core/vdr/src/EpgPersonIndexRepositoryMaintenance.cpp \
-        core/vdr/src/EpgPersonEnrichmentService.cpp \
+        core/vdr/src/EpgPersonEnrichmentService.cpp
+
+PERSON_CONTEXT_SRC := \
         core/vdr/src/PersonContextService.cpp \
         core/vdr/src/PersonContextJsonSerializer.cpp
 
 VDR_SRC := \
+        $(EPG_PERSON_INDEX_SRC) \
         core/vdr/src/VdrConfig.cpp \
         core/vdr/src/BackendRegistry.cpp \
         core/vdr/src/BackendRegistryService.cpp \
