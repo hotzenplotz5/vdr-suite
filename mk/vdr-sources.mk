69421f44f1385894849d85d3a6947c547349c6ec
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-07-21 11:04:59 +0200

    Build: isolate person context source ownership

--- mk/vdr-sources.mk
+++ mk/vdr-sources.mk
@@ -1,5 +1,12 @@
+PERSON_CONTEXT_SRC := \
+        core/metadata/src/MetadataPlatformSchemaInstaller.cpp \
+        core/vdr/src/EpgPersonIndexRepository.cpp \
+        core/vdr/src/EpgPersonIndexRepositoryMaintenance.cpp \
+        core/vdr/src/EpgPersonEnrichmentService.cpp \
+        core/vdr/src/PersonContextService.cpp \
+        core/vdr/src/PersonContextJsonSerializer.cpp
+
 VDR_SRC := \
-        $(METADATA_PLATFORM_SRC) \
         core/vdr/src/VdrConfig.cpp \
         core/vdr/src/BackendRegistry.cpp \
         core/vdr/src/BackendRegistryService.cpp \
@@ -23,11 +30,6 @@ VDR_SRC := \
         core/vdr/src/EpgScraperMetadataResolverRegistry.cpp \
         core/vdr/src/SuiteBridgeEpgMetadataResolver.cpp \
         core/vdr/src/EpgScraperMetadataPublicJsonSerializer.cpp \
-        core/vdr/src/EpgPersonIndexRepository.cpp \
-        core/vdr/src/EpgPersonIndexRepositoryMaintenance.cpp \
-        core/vdr/src/EpgPersonEnrichmentService.cpp \
-        core/vdr/src/PersonContextService.cpp \
-        core/vdr/src/PersonContextJsonSerializer.cpp \
         core/vdr/src/EpgQueryService.cpp \
         core/vdr/src/EpgSearchMatcher.cpp \
         core/vdr/src/EpgSearchResultJsonSerializer.cpp \
