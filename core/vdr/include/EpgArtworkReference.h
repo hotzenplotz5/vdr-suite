e845bcf93bd52d87d5b3e39bb45d10ecfdbba8f8
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-03 14:57:21 +0200

    feat(epg): persist fallback artwork provenance

--- core/vdr/include/EpgArtworkReference.h
+++ core/vdr/include/EpgArtworkReference.h
@@ -2,6 +2,13 @@
 
 #include <string>
 
+enum class EpgArtworkReferenceOrigin
+{
+    Unknown,
+    PrimaryMetadata,
+    ExternalFallback
+};
+
 struct EpgArtworkReference
 {
     std::string backendId;
@@ -9,6 +16,7 @@ struct EpgArtworkReference
     std::string eventId;
 
     std::string provider;
+    EpgArtworkReferenceOrigin origin = EpgArtworkReferenceOrigin::Unknown;
     std::string path;
     int width = 0;
     int height = 0;
