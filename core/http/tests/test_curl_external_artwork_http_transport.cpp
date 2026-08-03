2d5b5334dbdc478849706bf425f5ed0b435d193d
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-03 17:29:00 +0200

    Test exact TVmaze transport allowlist

--- core/http/tests/test_curl_external_artwork_http_transport.cpp
+++ core/http/tests/test_curl_external_artwork_http_transport.cpp
@@ -8,25 +8,47 @@ int main()
 {
     const std::set<std::string> hosts = {
         "api.themoviedb.org",
-        "image.tmdb.org"
+        "api.tvmaze.com",
+        "image.tmdb.org",
+        "static.tvmaze.com"
     };
 
     assert(CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
         "https://api.themoviedb.org/3/tv/1/images", hosts));
     assert(CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
         "https://image.tmdb.org/t/p/original/test.jpg", hosts));
+    assert(CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "https://api.tvmaze.com/lookup/shows?imdb=tt0944947", hosts));
+    assert(CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "https://api.tvmaze.com/shows/82/images", hosts));
+    assert(CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "https://static.tvmaze.com/uploads/images/original_untouched/1/4183.jpg",
+        hosts));
+
     assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
         "http://api.themoviedb.org/3/tv/1", hosts));
     assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "http://api.tvmaze.com/shows/1", hosts));
+    assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
         "https://evil.api.themoviedb.org/3/tv/1", hosts));
+    assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "https://evil.api.tvmaze.com/shows/1", hosts));
     assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
         "https://api.themoviedb.org.evil.example/3/tv/1", hosts));
     assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "https://static.tvmaze.com.evil.example/image.jpg", hosts));
+    assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
         "https://user@api.themoviedb.org/3/tv/1", hosts));
+    assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "https://user@api.tvmaze.com/shows/1", hosts));
     assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
         "https://api.themoviedb.org:443/3/tv/1", hosts));
     assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "https://api.tvmaze.com:443/shows/1", hosts));
+    assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
         "https://api.themoviedb.org\\evil", hosts));
+    assert(!CurlExternalArtworkHttpTransport::isAllowedHttpsUrl(
+        "https://api.tvmaze.com\\evil", hosts));
 
     CurlExternalArtworkHttpTransport transport;
     ExternalArtworkHttpRequest invalid;
@@ -36,6 +58,7 @@ int main()
     assert(response.transportError);
     assert(response.statusCode == 0);
     assert(response.body.empty());
+    assert(response.location.empty());
 
     invalid.url = "https://api.themoviedb.org/3/tv/1";
     invalid.bearerToken = "bad token";
