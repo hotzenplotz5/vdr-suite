8b3529c0f7973a9a1e85b4081745544166e4e2d3
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-04 13:51:10 +0200

    build(metadata): register poster materializer

--- mk/metadata-sources.mk
+++ mk/metadata-sources.mk
@@ -5,7 +5,8 @@ MANUAL_RECORDING_METADATA_SRC := \
 	core/metadata/src/ManualRecordingMetadataAssignmentRepository.cpp \
 	core/metadata/src/RecordingMetadataCandidateProvider.cpp \
 	core/metadata/src/TmdbRecordingMetadataCandidateJson.cpp \
-	core/metadata/src/TmdbRecordingMetadataCandidateProvider.cpp
+	core/metadata/src/TmdbRecordingMetadataCandidateProvider.cpp \
+	core/metadata/src/TmdbRecordingMetadataPosterMaterializer.cpp
 
 METADATA_GENRE_SRC := \
 	core/metadata/src/GenreIndexRepository.cpp
