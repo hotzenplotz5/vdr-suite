1558d4f6962b18d80f2763b152665d4184eef6fa
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-04 13:29:30 +0200

    refactor(recordings): isolate manual metadata facade

--- core/recordings/src/MetadataRepository.cpp
+++ core/recordings/src/MetadataRepository.cpp
@@ -109,36 +109,3 @@ std::optional<Metadata> MetadataRepository::getMetadataForRecording(int recordin
 
     return result;
 }
-
-bool MetadataRepository::assignManualRecordingMetadata(
-    const ManualRecordingMetadataSelection& selection,
-    ManualRecordingMetadataAssignment& assigned)
-{
-    ManualRecordingMetadataAssignmentRepository repository(database_);
-    return repository.assign(selection, assigned);
-}
-
-bool MetadataRepository::withdrawManualRecordingMetadata(
-    const std::string& backendId,
-    const std::string& resourceKey,
-    const std::string& actorRef,
-    int expectedRevision,
-    ManualRecordingMetadataAssignment& withdrawn)
-{
-    ManualRecordingMetadataAssignmentRepository repository(database_);
-    return repository.withdraw(
-        backendId,
-        resourceKey,
-        actorRef,
-        expectedRevision,
-        withdrawn);
-}
-
-ManualRecordingMetadataAssignment
-MetadataRepository::getManualRecordingMetadata(
-    const std::string& backendId,
-    const std::string& resourceKey)
-{
-    ManualRecordingMetadataAssignmentRepository repository(database_);
-    return repository.findSelected(backendId, resourceKey);
-}
