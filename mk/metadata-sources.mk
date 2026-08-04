d76337cfaad64eea0af5a8e99cefc62cbe85adc1
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-04 13:11:37 +0200

    build(metadata): register manual assignment repository

--- mk/metadata-sources.mk
+++ mk/metadata-sources.mk
@@ -1,5 +1,6 @@
 METADATA_PLATFORM_SRC := \
-	core/metadata/src/MetadataIdentity.cpp
+	core/metadata/src/MetadataIdentity.cpp \
+	core/metadata/src/ManualRecordingMetadataAssignmentRepository.cpp
 
 METADATA_GENRE_SRC := \
 	core/metadata/src/GenreIndexRepository.cpp
