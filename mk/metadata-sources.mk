1508d31ea28e494c590305c3a43443deac5163cd
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-04 13:16:49 +0200

    build(metadata): register recording candidate provider sources

--- mk/metadata-sources.mk
+++ mk/metadata-sources.mk
@@ -1,6 +1,11 @@
 METADATA_PLATFORM_SRC := \
-	core/metadata/src/MetadataIdentity.cpp \
-	core/metadata/src/ManualRecordingMetadataAssignmentRepository.cpp
+	core/metadata/src/MetadataIdentity.cpp
+
+MANUAL_RECORDING_METADATA_SRC := \
+	core/metadata/src/ManualRecordingMetadataAssignmentRepository.cpp \
+	core/metadata/src/RecordingMetadataCandidateProvider.cpp \
+	core/metadata/src/TmdbRecordingMetadataCandidateJson.cpp \
+	core/metadata/src/TmdbRecordingMetadataCandidateProvider.cpp
 
 METADATA_GENRE_SRC := \
 	core/metadata/src/GenreIndexRepository.cpp
