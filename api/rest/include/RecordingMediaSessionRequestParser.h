3eccfad1c3d434ef201dfaa6a89c03db984856ac
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-17 22:26:20 +0200

    Add recording media stop request contract

--- api/rest/include/RecordingMediaSessionRequestParser.h
+++ api/rest/include/RecordingMediaSessionRequestParser.h
@@ -13,9 +13,20 @@ struct RecordingMediaSessionRequest
     ClientMediaCapabilities capabilities;
 };
 
+struct RecordingMediaSessionStopRequest
+{
+    bool valid = false;
+    std::string reasonCode;
+    std::string backendId;
+    std::string sessionId;
+};
+
 class RecordingMediaSessionRequestParser
 {
 public:
     RecordingMediaSessionRequest parse(
+        const std::string& body) const;
+
+    RecordingMediaSessionStopRequest parseStop(
         const std::string& body) const;
 };
