3c21973b319d59dd836c36a42af9836522125bbc
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-18 12:43:00 +0200

    Add media calibrator policy regression

--- mk/media-transcode-policy.mk
+++ mk/media-transcode-policy.mk
@@ -1,4 +1,4 @@
-.PHONY: install-media-transcode-calibrator test-phase65-media-transcode-calibrator-install
+.PHONY: install-media-transcode-calibrator test-phase65-media-transcode-calibrator-install test-phase65-media-transcode-calibrator-policy
 
 # Keep the calibrator available with install-runtime as an operator tool. The
 # daemon itself never benchmarks during playback or startup.
@@ -8,6 +8,9 @@ install-media-transcode-calibrator:
 	$(INSTALL) -d $(DESTDIR)$(BINDIR)
 	$(INSTALL) -m 0755 tools/vdr_suite_media_calibrate.py \
 		$(DESTDIR)$(BINDIR)/vdr-suite-media-calibrate
+
+test-phase65-media-transcode-calibrator-policy:
+	python3 tools/tests/test_vdr_suite_media_calibrate.py
 
 test-phase65-media-transcode-calibrator-install:
 	python3 -m py_compile tools/vdr_suite_media_calibrate.py
