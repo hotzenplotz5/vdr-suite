4dc03d1b79fd5d90a0440823fecc057841308bb1
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-08-25 10:22:46 +0200

    Phase 65.D: define Recording WebVTT subtitle delivery

---
+++ core/media/include/RecordingSubtitleWebVtt.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "MediaCapabilities.h"
+
+#include <string>
+#include <vector>
+
+struct RecordingSubtitleWebVttPlan
+{
+    bool valid = false;
+    std::string reasonCode;
+    std::vector<std::string> argv;
+};
+
+class RecordingSubtitleWebVtt
+{
+public:
+    static bool supports(MediaSubtitleFormat format);
+
+    static RecordingSubtitleWebVttPlan build(
+        int sourceSubtitleStreamIndex,
+        MediaSubtitleFormat format);
+};
