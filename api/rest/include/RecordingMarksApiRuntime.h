58bc3b7b9f6c81ba89350a8fb721a70c591ad3c1
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-09-05 09:53:05 +0200

    Add protected recording marks mutation runtime contract

--- api/rest/include/RecordingMarksApiRuntime.h
+++ api/rest/include/RecordingMarksApiRuntime.h
@@ -23,6 +23,44 @@ struct RecordingMarksBackendAccess
     IVdrRecordingNativeMarksResolver* resolver = nullptr;
 };
 
+enum class RecordingMarksMutationKind
+{
+    Add,
+    Delete,
+    Move,
+    Reset,
+    Replace
+};
+
+struct RecordingMarksMutationRequest
+{
+    RecordingMarksMutationKind kind = RecordingMarksMutationKind::Add;
+    std::string operationId;
+    std::string operationRevision;
+    std::string recordingKey;
+    std::string expectedMarksRevision;
+    int sourceFrame = -1;
+    int targetFrame = -1;
+    std::vector<int> replacementFrames;
+    std::string backendId;
+};
+
+struct RecordingMarksMutationDispatchResult
+{
+    bool accepted = false;
+    bool replayed = false;
+    std::string reasonCode;
+    std::string commandId;
+    std::string requestFingerprint;
+};
+
+struct RecordingMarksBackendWriteAccess
+{
+    bool allowed = false;
+    int statusCode = 503;
+    std::string reasonCode = "recording_marks_backend_write_unavailable";
+};
+
 class RecordingMarksApiRuntime
 {
 public:
@@ -30,18 +68,31 @@ public:
         std::function<std::vector<VdrRecording>(const std::string& backendId)>;
     using BackendResolver =
         std::function<RecordingMarksBackendAccess(const std::string& backendId)>;
+    using BackendWritePolicy =
+        std::function<RecordingMarksBackendWriteAccess(const std::string& backendId)>;
+    using MutationDispatcher =
+        std::function<RecordingMarksMutationDispatchResult(
+            const RecordingMarksMutationRequest& request)>;
 
     static RecordingMarksApiRuntime& instance();
 
     bool configure(
         RecordingLookup recordingLookup,
-        BackendResolver backendResolver);
+        BackendResolver backendResolver,
+        BackendWritePolicy backendWritePolicy = {},
+        MutationDispatcher mutationDispatcher = {});
 
     void reset();
     bool configured() const;
+    bool mutationConfigured() const;
 
     bool tryHandleGet(
         const std::string& requestTarget,
+        ApiResponse& response) const;
+
+    bool tryHandlePost(
+        const std::string& requestTarget,
+        const std::string& body,
         ApiResponse& response) const;
 
 private:
@@ -50,4 +101,6 @@ private:
     mutable std::mutex mutex_;
     RecordingLookup recordingLookup_;
     BackendResolver backendResolver_;
+    BackendWritePolicy backendWritePolicy_;
+    MutationDispatcher mutationDispatcher_;
 };
