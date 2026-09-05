8004837d993c4b23c1bbcabf5ab267766f5eda53
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-09-05 09:54:54 +0200

    Wire marks mutation control-plane dependencies into daemon runtime

--- core/daemon/include/DaemonRuntimeRecordingMarks.h
+++ core/daemon/include/DaemonRuntimeRecordingMarks.h
@@ -6,8 +6,17 @@
 #include <memory>
 #include <vector>
 
+class BackendAccessPolicy;
+class BackendAgentCommandRepository;
+class BackendAgentRepository;
+class BackendRegistryService;
+
 bool configureDaemonRecordingMarksRuntime(
     VdrRecordingCacheRepository& recordingCacheRepository,
-    const std::vector<std::unique_ptr<BackendRuntimeContext>>& backendRuntimeContexts);
+    const std::vector<std::unique_ptr<BackendRuntimeContext>>& backendRuntimeContexts,
+    BackendRegistryService& backendRegistryService,
+    BackendAccessPolicy& backendAccessPolicy,
+    BackendAgentRepository& backendAgentRepository,
+    BackendAgentCommandRepository& backendAgentCommandRepository);
 
 void resetDaemonRecordingMarksRuntime();
