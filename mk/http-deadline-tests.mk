cf835a8165e2e9bb7e8d947b1bde2b8fad242409
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-09-08 20:32:14 +0200

    Include HTTP deadline regression in fast CI

--- mk/http-deadline-tests.mk
+++ mk/http-deadline-tests.mk
@@ -7,4 +7,8 @@ test-basic-http-client-deadline:
 		-o $(BUILD_DIR)/test_basic_http_client_deadline
 	$(BUILD_DIR)/test_basic_http_client_deadline
 
+# The HTTP recovery changes must be exercised by the required fast CI graph,
+# not only by the broader local test target.
+test-fast: test-basic-http-client-deadline
+
 test: test-basic-http-client-deadline
