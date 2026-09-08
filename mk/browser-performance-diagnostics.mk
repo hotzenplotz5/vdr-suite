e1e02113dfcff85db8d0119763949bdff05a022d
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-09-09 00:29:17 +0200

    Compile production HTTP routing in diagnostics regression gate

--- mk/browser-performance-diagnostics.mk
+++ mk/browser-performance-diagnostics.mk
@@ -14,6 +14,10 @@ test-browser-performance-diagnostics:
 	node tools/test_browser_artwork_probe.js
 	node tools/test_browser_performance_diagnostics.js
 	node tools/test_browser_diagnostics_http_routes.js
+	$(BUILD_CXX) $(CXXFLAGS) \
+		core/http/tests/test_browser_diagnostics_asset_routes.cpp \
+		-o $(BUILD_DIR)/test_browser_diagnostics_asset_routes
+	$(BUILD_DIR)/test_browser_diagnostics_asset_routes
 
 install-browser-performance-diagnostics:
 	$(INSTALL) -d $(DESTDIR)$(DATADIR)/web/frontend
