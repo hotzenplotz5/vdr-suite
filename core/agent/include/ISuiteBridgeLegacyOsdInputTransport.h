dedcb2e9a830bcaaf77975ff23515efa5fc047dd
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-09-21 17:05:55 +0200

    Phase 68.G: add typed SuiteBridge OSD input transport boundary

---
+++ core/agent/include/ISuiteBridgeLegacyOsdInputTransport.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "LegacyOsdInputDomain.h"
+#include "ISuiteBridgeLocalTransport.h"
+
+namespace vdrsuite::agent
+{
+
+class ISuiteBridgeLegacyOsdInputTransport
+{
+public:
+    virtual ~ISuiteBridgeLegacyOsdInputTransport() = default;
+
+    virtual SuiteBridgeCommandReply executeLegacyOsdInput(
+        const LegacyOsdInputCommand& command) = 0;
+};
+
+}
