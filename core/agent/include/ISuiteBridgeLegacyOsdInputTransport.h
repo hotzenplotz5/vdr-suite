3aed221efdfd07c9d59c4d3495fb44cc9603a295
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-09-21 17:11:15 +0200

    Phase 68.G: discovery-gate local OSD input capability

--- core/agent/include/ISuiteBridgeLegacyOsdInputTransport.h
+++ core/agent/include/ISuiteBridgeLegacyOsdInputTransport.h
@@ -11,6 +11,8 @@ class ISuiteBridgeLegacyOsdInputTransport
 public:
     virtual ~ISuiteBridgeLegacyOsdInputTransport() = default;
 
+    virtual bool legacyOsdInputAvailable() = 0;
+
     virtual SuiteBridgeCommandReply executeLegacyOsdInput(
         const LegacyOsdInputCommand& command,
         const std::string& requestFingerprint) = 0;
