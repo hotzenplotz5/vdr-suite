d1a9d6a4e88b29f3b88871d43b3972cf156e29e9
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-09-23 21:02:27 +0200

    Phase 69.C: link assignment desired Timer specification codec

--- mk/phase64-timer-assignment-fulfillment-tests.mk
+++ mk/phase64-timer-assignment-fulfillment-tests.mk
@@ -10,6 +10,7 @@ test-phase64-timer-assignment-fulfillment: test-phase64-timer-assignment-fulfill
 		core/timers/src/TimerIntentRepository.cpp \
 		core/timers/src/TimerAssignment.cpp \
 		core/timers/src/TimerAssignmentRepository.cpp \
+\t\tcore/timers/src/TimerAssignmentDesiredNativeTimerSpecification.cpp \
 		core/timers/src/NativeTimerBinding.cpp \
 		core/timers/src/NativeTimerBindingRepository.cpp \
 		core/timers/src/NativeTimerBindingReadRepository.cpp \
