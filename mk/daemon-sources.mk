ed34af0bb1b3ec506c3a6fe4df54d1f8cce373f8
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-09-24 08:55:06 +0200

    Phase 69.C: link Timer CREATE reconciliation sources

--- mk/daemon-sources.mk
+++ mk/daemon-sources.mk
@@ -21,6 +21,9 @@ DAEMON_SRC := \
         core/timers/src/NativeTimerCreateOperationPreparationService.cpp \
         core/timers/src/NativeTimerCreateAdmissionService.cpp \
         core/timers/src/NativeTimerCreateReadbackExpectation.cpp \
+        core/timers/src/NativeTimerCreateReadbackEvidence.cpp \
+        core/timers/src/NativeTimerCreateReadbackVerificationService.cpp \
+        core/timers/src/NativeTimerCreateOperationCompletionService.cpp \
         core/timers/src/NativeTimerCreateDispatchService.cpp \
         core/agent/src/BackendAgentCommandReservation.cpp \
         core/agent/src/BackendAgentNativeTimerCreateReservation.cpp \
